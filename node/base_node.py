a5b6f2ad5425b698de6f5052b369d362499643be
yite <yite@owl3d.ai>
2023-05-13 03:26:39 +0000

    add neighbor function

--- node/base_node.py
+++ node/base_node.py
@@ -1,11 +1,17 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import List, Type
 
 
 @dataclass()
 class BaseNode:
     name: str
+    neighbors: List['BaseNode'] = field(default_factory=list)
+
+    def add_neighbor(self, node: 'BaseNode'):
+        self.neighbors.append(node)
 
     def run(self):
         print(f'This is {self.name}')
+        print(f'Next node have')
         return 0
 
